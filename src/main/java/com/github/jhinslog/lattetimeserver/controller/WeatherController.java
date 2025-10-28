205d60660b9e76ac2fe040771e1552f70741a5f5
jhin <joojh501@gmail.com>
2025-10-29 05:42:39 +0900

    feat: 날씨 조회 API 엔드포인트 WeatherController 구현

---
+++ src/main/java/com/github/jhinslog/lattetimeserver/controller/WeatherController.java
@@ -0,0 +1,28 @@
+package com.github.jhinslog.lattetimeserver.controller;
+
+import com.github.jhinslog.kma.dto.ApiResponse;
+import com.github.jhinslog.lattetimeserver.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/weather")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @GetMapping("/ultra-srt-ncst")
+    public ApiResponse getUltraSrtNcst(
+            @RequestParam String baseDate,
+            @RequestParam String baseTime,
+            @RequestParam int nx,
+            @RequestParam int ny) throws IOException {
+        return weatherService.getUltraSrtNcst(baseDate, baseTime, nx, ny);
+    }
+}
