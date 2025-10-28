c261761126255cbd9ad80ef5bbbcdb4600badf4d
jhin <joojh501@gmail.com>
2025-10-29 05:42:49 +0900

    fix: DataSource 자동 설정 비활성화로 애플리케이션 실행 오류 해결

--- src/main/java/com/github/jhinslog/lattetimeserver/LatteTimeServerApplication.java
+++ src/main/java/com/github/jhinslog/lattetimeserver/LatteTimeServerApplication.java
@@ -2,8 +2,9 @@ package com.github.jhinslog.lattetimeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //DB 연결 제외
 public class LatteTimeServerApplication {
 
     public static void main(String[] args) {
